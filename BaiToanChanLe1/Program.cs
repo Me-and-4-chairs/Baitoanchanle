a967810b76454016e1ff497cf5ae89f98e97d9f6
diamo.pc <diamo.pc@DESKTOP-5OVSU0B>
2024-09-09 16:38:50 +0700

    Add project files.

---
+++ BaiToanChanLe1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiToanChanLe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap mot so nguyen: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} la so chan.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} la so le.");
+            }
+            Console.ReadKey();
+            //yutytutyu
+        }
+    }
+}
