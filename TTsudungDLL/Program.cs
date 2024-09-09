faf4f11899b766b760f98842dc153e198697fcfe
diamo.pc <diamo.pc@DESKTOP-5OVSU0B>
2024-09-09 16:47:45 +0700

    console su dung dll

---
+++ TTsudungDLL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using lib_chanle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        kiemtra checker = new kiemtra();
+
+        Console.Write("Nhap mot so nguyen: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string result = checker.Main(number);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
---
+++ lib_chanle/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib_chanle
+{
+    public class kiemtra
+    {
+        public string Main(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return $"{number} la so chan";
+            }
+            else
+            {
+                return $"{number} la so le.";
+            }
+        }
+    }
+}
